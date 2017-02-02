5bfb0d5bb680f1fef2eeac30f585df0c7fd473d6
Felipe Pardo Suarez <felipe.pardosuarez@gmail.com>
2017-02-02 21:58:55 +0000

    Seeded maps table

--- db/seeds/00_populate-users.js
+++ db/seeds/00_populate-users.js
@@ -5,8 +5,8 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex("users").insert({email: "ash@pokemon.com", password: "pikachu", first_name: "Ash", last_name: "Ketchum"}),
-        knex("users").insert({email: "gary@pokemon.com", password: "growlithe", first_name: "Gary", last_name: "Oak"}),
+        knex("users").insert({id: 1, email: "ash@pokemon.com", password: "pikachu", first_name: "Ash", last_name: "Ketchum"}),
+        knex("users").insert({id: 2, email: "gary@pokemon.com", password: "growlithe", first_name: "Gary", last_name: "Oak"})
       ]);
     });
 };
---
+++ db/seeds/01_populate-maps.js
@@ -0,0 +1,14 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+//console.log(knex.select("id").from("users").pluck("id"));
+
+  return knex("maps").del()
+    .then(function () {
+      return Promise.all([
+        // Inserts seed entries
+        knex("maps").insert({map_title: "Sushi Restaurants", created_by: 1}),
+        knex("maps").insert({map_title: "Gift Shops", created_by: 2})
+      ]);
+    });
+};
