9adc80c6e98329edf45e6fbaf941475befe4041e
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-02-02 20:55:01 +0000

    Added knex logic to users routes.

--- routes/users.js
+++ routes/users.js
@@ -16,18 +16,30 @@ module.exports = (knex) => {
   });
 
   // render user profile page
-  router.get("/:userid", (req, res) => {
-
+  router.get("/:user_id", (req, res) => {
+    res.render("user");
   });
 
   // return list of user's favourites
-  router.get("/:userid/favourites", (req, res) => {
-
+  router.get("/:user_id/favourites", (req, res) => {
+    knex
+      .select("map_id")
+      .from("favourites")
+      .where("user_id", req.session.user_id)
+      .then((results) => {
+        res.json(results);
+    });
   });
 
   // toggle existence of user/map favourite
-  router.post("/:userid/favourites", (req, res) => {
-
+  router.post("/:user_id/favourites", (req, res) => {
+    knex
+      .select(*)
+      .from("favourites")
+      .where("user_id", req.params.user_id).andWhere("map_id", req.body.map_id)
+      .then((results) => {
+        console.log(results);
+    });
   });
 
   return router;
