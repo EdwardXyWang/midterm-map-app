c7a554d2c361aa8e3db088135641e05cbd6f9826
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-02-03 01:27:50 +0000

    Added on click handler for links in map list. Changed buttons to links.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,17 @@
 $(() => {
   $.ajax({
     method: "GET",
-    url: "/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
+    url: "/maps"
+  }).done((maps) => {
+    for(map of maps) {
+      $("<a>").attr("href", "#").text(map.map_title).addClass("list-group-item").appendTo($(".map-list"));
     }
   });;
+
+
+
+  $(".map-list").on("click", "a", function () {
+    $(".maps-pane").addClass("hidden");
+    $(".points-pane").removeClass("hidden");
+  });
 });
