71f406433956b104d3962e34481f12c4c06e34e1
Felipe Pardo Suarez <felipe.pardosuarez@gmail.com>
2017-02-06 00:09:06 +0000

    Delete points file refac

--- public/scripts/delete-point.js
+++ public/scripts/delete-point.js
@@ -2,7 +2,7 @@ $(() => {
 
   $(".point-delete-btn").on("click", function() {
     const pointToDelteId = $(".point-edit-btn").data().pointId;
-
+    console.log(pointToDelteId);
 
     $.ajax({
       method: "POST",
